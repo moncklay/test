89d44606c18ba2a455af767bf20a647e3d45f9b2
moncklay <homoko@bk.ru>
2017-09-27 20:05:39 +0300

    some style

--- lib/test.rb
+++ lib/test.rb
@@ -21,12 +21,12 @@ class Test
 
   def answers_score(user_input)
     case user_input
-      when "да"
-        score(2)
-      when "иногда"
-        score(1)
-      else
-        score(0)
+    when "да"
+      score(2)
+    when "иногда"
+      score(1)
+    else
+      score(0)
     end
   end
 
@@ -40,20 +40,20 @@ class Test
 
   def result(results)
     case @score
-      when 0..3
-        results[6]
-      when 4..8
-        results[5]
-      when 9..13
-        results[4]
-      when 14..18
-        results[3]
-      when 19..24
-        results[2]
-      when 25..29
-        results[1]
-      else
-        results[0]
+    when 0..3
+      results[6]
+    when 4..8
+      results[5]
+    when 9..13
+      results[4]
+    when 14..18
+      results[3]
+    when 19..24
+      results[2]
+    when 25..29
+      results[1]
+    else
+      results[0]
     end
   end
 end
